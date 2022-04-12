0c8db32c14a449f7a0fe5739b1f3d68689313543
AntoNunez <a.nunez.jeria@gmail.com>
2022-04-12 15:33:52 +0000

    Random Card

--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let cartas = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let tipos = ["diamond", "heart", "suit", "spade"];
+  let numero = document.querySelector(".numero");
+  let button = document.getElementById("button");
+
+  numero.innerHTML = cartas[Math.floor(Math.random() * cartas.length)];
+  numero.classList.add(tipos[Math.floor(Math.random() * tipos.length)]);
 };
